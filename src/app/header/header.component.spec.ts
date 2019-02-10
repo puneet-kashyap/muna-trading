694e353a5efe7f82c2b6e831a72165fa517b42a7
Puneet Kashyap <pkashyaps@gmail.com>
2019-02-10 13:59:57 -0500

    Unit  test for header component added

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -9,9 +9,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      declarations: [HeaderComponent],
+      imports: [RouterTestingModule]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -25,7 +25,21 @@ describe('HeaderComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // it('should have company name in header', () => {
-  //   expect(compiled.querySelector('h1').textContent).toContain('Muna Trading Co.');
-  // });
+  it('should have company name in header', () => {
+    expect(compiled.querySelector('h1').textContent).toContain(component.companyName);
+    expect(compiled.querySelector('h1>a').getAttribute('href')).toContain('/');
+  });
+
+  it('should have tag line in header', () => {
+    expect(compiled.querySelector('#tag-line').textContent).toContain(component.tagLine);
+  });
+
+  it('should have all the correct navigation routes in header', () => {
+    const routes = compiled.querySelectorAll('.navbar>a');
+    expect(routes.length).toEqual(component.routes.length);
+    routes.forEach((x, index) => {
+      expect(x.textContent).toContain(component.routes[index].link);
+      expect(x.getAttribute('href')).toContain(component.routes[index].url);
+    });
+  });
 });
