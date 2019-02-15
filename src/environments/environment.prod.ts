64e7b10ee7711b9b2c6014085bdde72164ad19cc
Puneet Kashyap <pkashyaps@gmail.com>
2019-02-15 09:32:45 -0500

    Firebase module added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDO5PT2Ysxcy9NaxO8iKBBOrJpKUv2OA68",
+    authDomain: "munatrading-fc632.firebaseapp.com",
+    projectId: "munatrading-fc632"
+  }
 };
